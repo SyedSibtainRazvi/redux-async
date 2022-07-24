fe3629f93315798274813d42cda4fb25297407a8
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-07-25 00:04:36 +0530

    feat: cart actions added

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,36 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const Dummy_Products = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My first book",
+    description: "This is my first book",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "My Second book",
+    description: "This is my second book",
+  }
+]
+
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {Dummy_Products.map(product => (
+          <ProductItem
+            key={product.key}
+            title={product.title}
+            id = {product.id}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
